90fdd6b8fbd186e71abc99fc2921bbb8f6db9b3b
v-Sharan <vsharansago@gmail.com>
2023-07-31 14:45:49 +0530

    added grafbase

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,34 @@
-import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import { NavLinks } from "@/constants";
+import { AuthProviders } from ".";
+
 const Navbar = () => {
+  const session = {};
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10 ">
         <Link href={"/"}>
           <Image src="/logo.svg" width={155} height={43} alt="Flexibble" />
         </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            User Photo
+            <Link href={"/create-post"}>Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
